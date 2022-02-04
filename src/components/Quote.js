3ebce0597078e129c32ffc41357beb951a2671f2
jamesfricker <jimmyf98@hotmail.com>
2022-02-04 17:54:00 +1100

    setup file structure

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+
+  
+  import React, { Component } from 'react';
+
+  export default class Quote extends Component {
+  
+    render() {
+    const quote = "This is a quote";
+      return(
+        <div>{quote}</div>
+        )
+    }
+  
+  }
---
+++ src/components/Stopwatch.js
@@ -0,0 +1,14 @@
+
+  
+  import React, { Component } from 'react';
+
+  export default class Stopwatch extends Component {
+  
+    render() {
+    const stopWatch = "Stopwatch";
+      return(
+        <div>{stopWatch}</div>
+        )
+    }
+  
+  }
